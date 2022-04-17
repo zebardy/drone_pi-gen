b5f893fa5aab69c5467b55f54d0fa12bba5c9725
Aaron <zebardy@gmail.com>
2022-04-17 11:46:46 +0100

    list directroy to help investigate where output goes

--- script.sh
+++ script.sh
@@ -11,3 +11,4 @@ echo "ENABLE_SSH='${PLUGIN_ENABLE_SSH}'" >> config
 touch /pi-gen/stage3/SKIP /pi-gen/stage4/SKIP /pi-gen/stage5/SKIP
 touch /pi-gen/stage4/SKIP_IMAGES /pi-gen/stage5/SKIP_IMAGES
 sudo /pi-gen/build.sh
+ls -lrt 
