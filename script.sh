0adeb5b13e06b947f4e547c3e1ea4372b50ddca1
Aaron <zebardy@gmail.com>
2022-04-18 10:50:59 +0100

    sudo not in container, should have enough permissions so removing sudo command from script

--- script.sh
+++ script.sh
@@ -10,5 +10,5 @@ echo "ENABLE_SSH='${PLUGIN_ENABLE_SSH}'" >> config
 
 touch /pi-gen/stage3/SKIP /pi-gen/stage4/SKIP /pi-gen/stage5/SKIP
 touch /pi-gen/stage4/SKIP_IMAGES /pi-gen/stage5/SKIP_IMAGES
-sudo /pi-gen/build.sh
+/pi-gen/build.sh
 ls -lrt 
