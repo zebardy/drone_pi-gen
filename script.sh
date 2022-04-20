eae1578c6a52f1f94226e39da2ea151451b09b54
Aaron <zebardy@gmail.com>
2022-04-20 11:06:28 +0100

    try starting the dind dockerd:wq

--- script.sh
+++ script.sh
@@ -3,10 +3,12 @@
 WORKSPACE=$(pwd)
 pwd
 ls -lrt
+
+/usr/local/bin/dockerd --host=unix:///var/run/docker.sock --data-root /var/lib/docker
 ls -lrt /var/run
-ls -lrt /proc
-sestatus
-setenforce 0
+#ls -lrt /proc
+#sestatus
+#setenforce 0
 docker ps
 
 echo "IMG_NAME='Raspbian'" > config
@@ -15,7 +17,8 @@ echo "ENABLE_SSH='${PLUGIN_ENABLE_SSH}'" >> config
 
 touch /pi-gen/stage3/SKIP /pi-gen/stage4/SKIP /pi-gen/stage5/SKIP
 touch /pi-gen/stage4/SKIP_IMAGES /pi-gen/stage5/SKIP_IMAGES
-/pi-gen/build-docker.sh
+ls -lrt /pi-gen/
+exec /pi-gen/build-docker.sh
 cat /pi-gen/work/Raspbian/stage0/rootfs/debootstrap/debootstrap.log
 
 ls -lrt 
