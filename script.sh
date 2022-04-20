398b91e4efd7a90b5091219592f10a3e46476968
Aaron <zebardy@gmail.com>
2022-04-20 20:40:05 +0100

    cleanup and try removing stage4/EXPORT* files to prevent attempting to look mount files that don't exist

--- script.sh
+++ script.sh
@@ -1,33 +1,28 @@
 #!/bin/sh
 
 WORKSPACE=$(pwd)
+PIGEN_DIR="${1:-/pi-gen}"
 pwd
 ls -lrt
 
-nohup /usr/local/bin/dockerd --host=unix:///var/run/docker.sock --data-root /var/lib/docker &
-sleep 15
-ls -lrt /var/run
-#ls -lrt /proc
-#sestatus
-#setenforce 0
-docker ps
+if [ ! -e /var/run/docker.sock ]; then
+  nohup /usr/local/bin/dockerd --host=unix:///var/run/docker.sock --data-root /var/lib/docker 2>&1 >/dev/null &
+  sleep 15
+fi
 
-cd /pi-gen
+cd $PIGEN_DIR
 echo "IMG_NAME='Raspbian'" > config
 echo "FIRST_USER_PASS='${PLUGIN_FIRST_USER_PASS}'" >> config
 echo "ENABLE_SSH='${PLUGIN_ENABLE_SSH}'" >> config
 
 touch ./stage3/SKIP ./stage4/SKIP ./stage5/SKIP
 touch ./stage4/SKIP_IMAGES ./stage5/SKIP_IMAGES
-ls -lrt ./build-docker.sh
-echo "This is a test"
-cat ./build-docker.sh
+rm stage4/EXPORT*
+
 exec ./build-docker.sh
-cat ./work/Raspbian/stage0/rootfs/debootstrap/debootstrap.log
+#cat ./work/Raspbian/stage0/rootfs/debootstrap/debootstrap.log
 
 ls -lrt
 ls -lrt ./deploy
 
 cd $WORKSPACE
-
-ls -lrt 
