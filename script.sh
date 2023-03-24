1de51d271b0204fb771ed73511b093363d9776ef
zebardy <zebardy@gmail.com>
2023-03-24 12:27:19 +0000

    output config data in logs if debug set
    Make the script slightly more verbose

--- script.sh
+++ script.sh
@@ -21,13 +21,21 @@ if [ -n "$PLUGIN_PUBKEY_ONLY_SSH" ]; then echo "PUBKEY_ONLY_SSH='${PLUGIN_PUBKEY
 if [ -n "$PLUGIN_ENABLE_SSH" ]; then echo "ENABLE_SSH='${PLUGIN_ENABLE_SSH}'" >> config fi
 if [ -n "$PLUGIN_TARGET_HOSTNAME" ]; then echo "TARGET_HOSTNAME='${PLUGIN_TARGET_HOSTNAME}'" >> config fi
 
+if [ -n $PLUGIN_DEBUG ]; then
+  echo "===CONFIG==="
+  cat config
+  echo "===CONFIG END==="
+fi
+
 #touch ./stage3/SKIP ./stage4/SKIP ./stage5/SKIP
 #touch ./stage4/SKIP_IMAGES ./stage5/SKIP_IMAGES
 
 if [ -f "./stage4/SKIP_IMAGES" ]; then
+  echo "WARNING: stage 4 SKIP_IMAGES set so skipping stage 4 EXPORT"
   rm stage4/EXPORT*
 fi
 
+echo "Docker based build start!!!"
 ./build-docker.sh
 #cat ./work/Raspbian/stage0/rootfs/debootstrap/debootstrap.log
 echo "Docker build done!!!"
